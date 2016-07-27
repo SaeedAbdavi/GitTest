fafa408f03130671b07f7feb77b52b89bdeaea3f
Saeed Abdavi <abdavi.saeed@gmail.com>
2016-07-27 17:09:39 +0430

    remove num

--- MyApplication/app/src/main/java/projects/hasin/com/myapplication/source/TestClass.java
+++ MyApplication/app/src/main/java/projects/hasin/com/myapplication/source/TestClass.java
@@ -4,7 +4,6 @@ package projects.hasin.com.myapplication.source;
  * Created by abdavi on 7/27/2016.
  */
 public class TestClass {
-    private int number;
     public static  void main(String[] args){
         System.out.println();
     }
